7be11a82df8dd710cde7fb7e87f75c035a3f3964
Terence Faid JABO <faidterence@outlook.com>
2024-04-18 18:08:26 +0200

    feat(database):connect to Database

---
+++ src/config/connectDB.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client"
+
+
+const prisma = new PrismaClient()
+
+
+export const connectDB = async () => {
+    try {
+        await prisma.$connect()
+        console.log("Database connected")
+    } catch (error) {
+        console.log("Database connection failed")
+        console.log(error)
+    }
+    }
