70158d1b639fb23b7e6cce4c9d5782687caa18d2
Terence Faid JABO <faidterence@outlook.com>
2024-05-01 21:28:05 +0200

    feat(comment 1): comment 1
    feat(comment 1): comment 2
    
    feat(comment 1): comment 3

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -11,4 +11,12 @@ router.get("/:id", jobController.getJobById);
 router.put("/:id", jobController.updateJob);
 router.delete("/:id", jobController.deleteJob);
 
+
+
+// comment 1
+
+// comment 2
+
+// comment 3
+
 export default router;
